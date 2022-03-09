30e658fabb99032529d43ee618a1a20621da48f7
Deepak Kumar <dpkjswl7@gmail.com>
2022-03-09 03:11:12 +0000

    Add all

---
+++ Video_index/get-videos.ps1
@@ -0,0 +1,9 @@
+$account_id="1bdb012d-71d1-42fb-9b75-7b46ebc3325a"
+$api_key="b284949fe09b405988ae19cfcef16752"
+$location="trial"
+
+# Call the AccessToken method with the API key in the header to get an access token
+$token = Invoke-RestMethod -Method "Get" -Uri "https://api.videoindexer.ai/auth/$location/Accounts/$account_id/AccessToken" -Headers @{'Ocp-Apim-Subscription-Key' = $api_key}
+
+# Use the access token to make an authenticated call to the Videos method to get a list of videos in the account
+Invoke-RestMethod -Method "Get" -Uri "https://api.videoindexer.ai/$location/Accounts/$account_id/Videos?accessToken=$token" | ConvertTo-Json
